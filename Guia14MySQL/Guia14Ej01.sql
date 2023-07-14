9fa763979a189e1c76c488646af6d2cc5c6db01b
Cristian Hoffmann <cristianhof@gmail.com>
2023-07-14 13:46:19 +0200

    ex03

--- Guia14MySQL/Guia14Ej01.sql
+++ Guia14MySQL/Guia14Ej01.sql
@@ -64,19 +64,35 @@ select * from personal.empleados where comision_emp > sal_emp;
 /*17. Listar los empleados cuya comisión es menor o igual que el 30% de su sueldo.*/
 select * from personal.empleados where comision_emp <= (sal_emp * 0.3);
 /*18. Hallar los empleados cuyo nombre no contiene la cadena “MA”*/
-select nombre from personal.empleados where nombre
+select nombre from personal.empleados where nombre not like '%MA%';
 /*19. Obtener los nombres de los departamentos que sean “Ventas”, “Investigación” o
-‘Mantenimiento.
+‘Mantenimiento.*/
+select * from personal.departamentos where nombre_depto = 'ventas' or nombre_depto = 'investigacion' or nombre_depto = 'mantenimiento';
 /*20. Ahora obtener el contrario, los nombres de los departamentos que no sean “Ventas” ni
-“Investigación” ni ‘Mantenimiento.
-/*21. Mostrar el salario más alto de la empresa.
-/*22. Mostrar el nombre del último empleado de la lista por orden alfabético.
-/*23. Hallar el salario más alto, el más bajo y la diferencia entre ellos.
-/*24. Hallar el salario promedio por departamento.
-Consultas con Having
+“Investigación” ni ‘Mantenimiento.*/
+select * from personal.departamentos where not nombre_depto = 'ventas' and not nombre_depto = 'investigacion' and not nombre_depto = 'mantenimiento';
+/*21. Mostrar el salario más alto de la empresa.*/
+select max(sal_emp) from personal.empleados;
+/*22. Mostrar el nombre del último empleado de la lista por orden alfabético.*/
+select nombre from personal.empleados order by nombre desc limit 1;
+/*23. Hallar el salario más alto, el más bajo y la diferencia entre ellos.*/
+select max(sal_emp),min(sal_emp),(max(sal_emp) - min(sal_emp)) as diferencia from personal.empleados;
+/*24. Hallar el salario promedio por departamento.*/
+select id_depto,avg(sal_emp) from personal.empleados group by id_depto;
+
+/*Consultas con Having*/
+
 /*25. Hallar los departamentos que tienen más de tres empleados. Mostrar el número de
-empleados de esos departamentos.
-/*26. Hallar los departamentos que no tienen empleados
-Consulta con Subconsulta
+empleados de esos departamentos.*/
+select nombre_depto, count(id_depto) as cantidad_empleados from personal.departamentos group by nombre_depto having count(id_depto) >= 3;
+
+/*26. Hallar los departamentos que no tienen empleados*/
+select nombre_depto, count(id_depto) as cantidad_empleados from personal.departamentos group by nombre_depto having count(id_depto) = 0;
+
+/*Consulta con Subconsulta*/
+
 /*28. Mostrar la lista de los empleados cuyo salario es mayor o igual que el promedio de la
 empresa. Ordenarlo por departamento.*/
+
+select nombre,sal_emp from personal.empleados where sal_emp >= (select avg(sal_emp) from personal.empleados);
+select avg(sal_emp) from personal.empleados;
